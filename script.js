ec89a6799b4f2638ae4f28dce32e93a66d0ce582
Liz Davidson <lizdavidson45@gmail.com>
2019-02-05 16:03:49 -0500

    add game logic to generate a new game from a preset list of words

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+
+class Game {
+    constructor (word) {
+        this.word = word;
+        this.letters = word.split("");
+    }
+}
+
+const gameWords = ["doctor","elephant","recreation","graph","class","literal","wheel","mineral","earring"]
+
+let currentGame = {}
+
+const createNewGame = function (){
+    let wordSelector = Math.floor(Math.random() * (gameWords.length-1))
+    currentGame = new Game(gameWords[wordSelector])
+}
+
