c2d51f7f1f0d2ad38dcff13322710b537399e975
Liz Davidson <lizdavidson45@gmail.com>
2019-02-05 17:19:58 -0500

    added method to class Game to shuffle the letters. called that method when assigning a new currentGame

--- script.js
+++ script.js
@@ -3,29 +3,52 @@ const gameWords = ["doctor","elephant","recreation","graph","class","literal","w
 const newGameButton = document.querySelector(".new-game")
 const letterContainer = document.querySelector(".letters-container")
 
+
+// shuffle array code from https://www.jstips.co/en/javascript/shuffle-an-array/
+
 class Game {
     constructor (word) {
         this.word = word;
         this.letters = word.split("");
     }
+    shuffle () {
+        var i;
+        var j;
+        var temp;
+        for (i = this.letters.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = this.letters[i];
+            this.letters[i] = this.letters[j];
+            this.letters[j] = temp;
+        }
+
+    }
 }
 
 // this function and variables are related to the creation of a new game. the "New Gmae Button" triggers this function to assign a new currentGame
+// createNewGame () steps:
+// 1. Clear the div with the letter tiles
+// 2. picks a random word from the gameWords array
+// 3. currentGame = new instance of Game based on random word
+// 4. calls the shuffle method on the new instance of Game
+// 5. for each letter in the currentGame, create a div with the class 'letter'
+// 6. append each letter into the div
+// 7. appends each div into the letters container element
 
 let currentGame = {}
 
 const createNewGame = function (){
     letterContainer.innerHTML = "";
     let wordSelector = Math.floor(Math.random() * (gameWords.length-1))
-    currentGame = new Game(gameWords[wordSelector])
+    currentGame = new Game(gameWords[wordSelector]);
+    currentGame.shuffle();
     console.log(currentGame)
     for (let i = 0; i < currentGame.letters.length; i++) {
-    let letterBlock = document.createElement('div')
+        let letterBlock = document.createElement('div')
     letterBlock.classList.add('letter')
-
     letterBlock.append(currentGame.letters[i])
     letterContainer.appendChild(letterBlock)
-    }
+}
 }
 
 // add event listener to the button that generates a new game
