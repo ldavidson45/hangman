9f9ea029220edbf7a71d8c7a7b35aa4e129c8f2b
Liz Davidson <lizdavidson45@gmail.com>
2019-02-05 16:13:01 -0500

    add button to set a new currentGame

--- script.js
+++ script.js
@@ -13,5 +13,11 @@ let currentGame = {}
 const createNewGame = function (){
     let wordSelector = Math.floor(Math.random() * (gameWords.length-1))
     currentGame = new Game(gameWords[wordSelector])
+    console.log(currentGame)
 }
 
+// add event listener to the button that generates a new game
+
+const newGameButton = document.querySelector(".new-game")
+
+newGameButton.addEventListener('click', createNewGame)
